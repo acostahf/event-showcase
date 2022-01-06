10dce097eb51531a8b43314044eef48f7244b198
Fabian <acostahf4@gmail.com>
2022-01-06 14:50:47 -0600

    create the events component to render out events passed in through a cms. Design is not final

---
+++ pages/components/events.js
@@ -0,0 +1,44 @@
+const LiItem = (props) => (
+  <li className="flex space-x-2">
+    <div>
+      <h4 className="font-bold text-sm">When</h4>
+      <div className="text-sm font-bold tetxt-gray-700 px-2 py-1 bg-blue-300 rounded transition-colors duration-300">
+        {props.when}
+      </div>
+    </div>
+    <div>
+      <h4 className="font-bold text-sm">Where</h4>
+      <div className="text-sm font-bold tetxt-gray-700 px-2 py-1 bg-blue-300 rounded transition-colors duration-300">
+        {props.where}
+      </div>
+    </div>
+    <div>
+      <h4 className="font-bold text-sm">What</h4>
+      <div className="text-sm font-bold tetxt-gray-700 px-2 py-1 bg-blue-300 rounded transition-colors duration-300">
+        {props.what}
+      </div>
+    </div>
+  </li>
+);
+
+export default () => (
+  <div>
+    <div className="text-center md:text-left md:flex max-w-3xl mx-auto mt-24">
+      <h1 className="font-bold text-6xl leading-tight">Events</h1>
+    </div>
+    <div className=" text-center md:text-left max-w-3xl mx-auto mt-5">
+      <ul className=" space-y-4 items-center">
+        <LiItem
+          when="tuesday"
+          where="Austin, Tx"
+          what="$2 fronzen margaritas. 8 flavors. Available drive-thru, to-go and dine-in."
+        />
+        <LiItem
+          when="tuesday"
+          where="Austin, Tx"
+          what="$2 fronzen margaritas. 8 flavors. Available drive-thru, to-go and dine-in."
+        />
+      </ul>
+    </div>
+  </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Events from "./components/events";
 import Navbar from "./components/navbar";
 
 export default () => (
   <div>
     <Navbar />
+    <Events />
   </div>
 );
